1d427ea6b627bfec12039c4d4c010075737336c3
Adrian Valcarcel-Schott <avalsch@proton.me>
2023-12-06 08:49:01 -0500

    day-1-part2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -3,17 +3,45 @@ fn main() {
 
     let res: u32 = input
         .lines()
-        .map(|line| {
-            line.chars()
-                .filter_map(|c| c.to_digit(10))
-                .collect::<Vec<_>>()
-        })
-        .map(|v| match v.as_slice() {
-            [f] => 11 * f,
-            [f, .., l] => 10 * f + l,
-            _ => unreachable!(),
-        })
+        .map(parse_digits)
+        .map(|digits| digits.first().unwrap() * 10 + digits.last().unwrap())
         .sum();
 
     println!("{res}");
+}
+
+fn parse_digits(line: &str) -> Vec<u32> {
+    let mut digits: Vec<u32> = vec![];
+    let mut index = 0;
+
+    while index < line.len() {
+        let digit = line
+            .chars()
+            .nth(index)
+            .unwrap()
+            .to_digit(10)
+            .unwrap_or_else(|| {
+                let sub = &line[index..];
+                match sub {
+                    _ if sub.starts_with("one") => 1,
+                    _ if sub.starts_with("two") => 2,
+                    _ if sub.starts_with("three") => 3,
+                    _ if sub.starts_with("four") => 4,
+                    _ if sub.starts_with("five") => 5,
+                    _ if sub.starts_with("six") => 6,
+                    _ if sub.starts_with("seven") => 7,
+                    _ if sub.starts_with("eight") => 8,
+                    _ if sub.starts_with("nine") => 9,
+                    _ => 0,
+                }
+            });
+
+        if digit != 0 {
+            digits.push(digit);
+        }
+
+        index += 1;
+    }
+
+    digits
 }
