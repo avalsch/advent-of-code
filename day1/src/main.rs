0dfceafb1ecf9c7adfd428b819ca302c5812bed9
Adrian Valcarcel-Schott <avalsch@proton.me>
2023-12-08 09:33:49 -0500

    day-1-part2-cleanup

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -12,35 +12,28 @@ fn main() {
 
 fn parse_digits(line: &str) -> Vec<u32> {
     let mut digits: Vec<u32> = vec![];
-    let mut index = 0;
 
-    while index < line.len() {
-        let digit = line
-            .chars()
-            .nth(index)
-            .unwrap()
-            .to_digit(10)
-            .unwrap_or_else(|| {
-                let sub = &line[index..];
-                match sub {
-                    _ if sub.starts_with("one") => 1,
-                    _ if sub.starts_with("two") => 2,
-                    _ if sub.starts_with("three") => 3,
-                    _ if sub.starts_with("four") => 4,
-                    _ if sub.starts_with("five") => 5,
-                    _ if sub.starts_with("six") => 6,
-                    _ if sub.starts_with("seven") => 7,
-                    _ if sub.starts_with("eight") => 8,
-                    _ if sub.starts_with("nine") => 9,
-                    _ => 0,
-                }
-            });
+    for sub in (0..line.len()).map(|i| &line[i..]) {
+        let digit = sub.chars().next().unwrap().to_digit(10).or_else(|| {
+            let digit = match sub {
+                _ if sub.starts_with("one") => 1,
+                _ if sub.starts_with("two") => 2,
+                _ if sub.starts_with("three") => 3,
+                _ if sub.starts_with("four") => 4,
+                _ if sub.starts_with("five") => 5,
+                _ if sub.starts_with("six") => 6,
+                _ if sub.starts_with("seven") => 7,
+                _ if sub.starts_with("eight") => 8,
+                _ if sub.starts_with("nine") => 9,
+                _ => return None,
+            };
 
-        if digit != 0 {
+            Some(digit)
+        });
+
+        if let Some(digit) = digit {
             digits.push(digit);
         }
-
-        index += 1;
     }
 
     digits
