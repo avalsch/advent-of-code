a1803a4618f8a4c34085b8847db93fc1ba37ebdd
Adrian Valcarcel-Schott <avalsch@proton.me>
2023-12-08 23:38:58 -0500

    day-2-part2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,31 +1,50 @@
+#[derive(Default)]
+struct Game {
+    red: u32,
+    green: u32,
+    blue: u32,
+}
+
 fn main() {
     let input = std::fs::read_to_string("input").unwrap();
 
-    let res: usize = input
+    let res: u32 = input
         .lines()
-        .enumerate()
-        .filter(|(_, game)| valid_game(game))
-        .map(|(i, _)| i + 1)
+        .map(parse_game_maxs)
+        .map(|Game { red, green, blue }| red * green * blue)
         .sum();
 
     println!("{res}");
 }
 
-fn valid_game(game: &str) -> bool {
+fn parse_game_maxs(game: &str) -> Game {
     let [_, records] = &game.split(':').collect::<Vec<_>>()[..] else {
         unreachable!()
     };
 
-    records.split([',', ';']).all(|record| {
-        let [count, color] = &record.split_whitespace().collect::<Vec<_>>()[..] else {
-            unreachable!()
-        };
+    records
+        .split([',', ';'])
+        .fold(Game::default(), |game, record| {
+            let [count, color] = &record.split_whitespace().collect::<Vec<_>>()[..] else {
+                unreachable!()
+            };
 
-        let count = count.parse::<u32>().unwrap();
+            let count = count.parse().unwrap();
 
-        matches!(
-            (count, *color),
-            (..=12, "red") | (..=13, "green") | (..=14, "blue")
-        )
-    })
+            match *color {
+                "red" => Game {
+                    red: game.red.max(count),
+                    ..game
+                },
+                "green" => Game {
+                    green: game.green.max(count),
+                    ..game
+                },
+                "blue" => Game {
+                    blue: game.blue.max(count),
+                    ..game
+                },
+                _ => unreachable!(),
+            }
+        })
 }
