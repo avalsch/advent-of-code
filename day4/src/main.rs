04e51cd95ace7f16305420ab4e6a0862665f17d7
Adrian Valcarcel-Schott <87724551+avalsch@users.noreply.github.com>
2023-12-09 20:22:08 -0500

    day-4-part2

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,30 +1,32 @@
 fn main() {
     let input = std::fs::read_to_string("input").unwrap();
 
-    let res: u32 = input.lines().map(card_points).sum();
+    let mut copies = (0..input.lines().count()).map(|_| 1).collect::<Vec<_>>();
+
+    for (i, card) in input.lines().enumerate() {
+        let card_copies = copies[i];
+
+        let [_card, numbers, winners] = &card.split([':', '|']).collect::<Vec<_>>()[..] else {
+            unreachable!()
+        };
+
+        let winners = winners
+            .split_whitespace()
+            .map(|n| n.parse().unwrap())
+            .collect::<Vec<u32>>();
+
+        let winners = numbers
+            .split_whitespace()
+            .map(|n| n.parse().unwrap())
+            .filter(|n| winners.contains(n))
+            .count();
+
+        copies[(i+1)..=(i + winners)]
+            .iter_mut()
+            .for_each(|c| *c += card_copies);
+    }
+
+    let res: usize = copies.iter().sum();
 
     println!("{res}");
-}
-
-fn card_points(card: &str) -> u32 {
-    let [_card, numbers, winners] = &card.split([':', '|']).collect::<Vec<_>>()[..] else {
-        unreachable!()
-    };
-
-    let winners = winners
-        .split_whitespace()
-        .map(|n| n.parse().unwrap())
-        .collect::<Vec<u32>>();
-
-    let shift = numbers
-        .split_whitespace()
-        .map(|n| n.parse().unwrap())
-        .filter(|n| winners.contains(n))
-        .count();
-
-    if shift > 0 {
-        1 << (shift - 1)
-    } else {
-        0
-    }
 }
