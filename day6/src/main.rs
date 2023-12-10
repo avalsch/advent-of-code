d9f2c2f9a6be94c9fc999c50f799ba417a69e955
Adrian Valcarcel-Schott <87724551+avalsch@users.noreply.github.com>
2023-12-10 01:18:08 -0500

    day-6-part2

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,29 +1,23 @@
 fn main() {
     let input = std::fs::read_to_string("input").unwrap();
 
-    let [times, distances] = &input
+    let [time, distance] = input
         .lines()
         .map(|line| {
             line.split_whitespace()
                 .skip(1)
-                .map(|n| n.parse::<u32>().unwrap())
-                .collect::<Vec<_>>()
+                .map(|n| n.parse::<u64>().unwrap())
+                .fold(0, |aggr, n| format!("{aggr}{n}").parse::<u64>().unwrap())
         })
         .collect::<Vec<_>>()[..]
     else {
         unreachable!()
     };
 
-    let records = times.iter().zip(distances);
-
-    let res: usize = records
-        .map(|(&time, &distance)| {
-            (0..time)
-                .map(move |t| t * time.abs_diff(t))
-                .filter(move |d| *d > distance)
-                .count()
-        })
-        .product();
+    let res: usize = (0..time)
+        .map(move |t| t * time.abs_diff(t))
+        .filter(move |d| *d > distance)
+        .count();
 
     println!("{res}");
 }
